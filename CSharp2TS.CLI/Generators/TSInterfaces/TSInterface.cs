7dff00ef2a77655dd00461eb6f4947a46130ae6a
Maxime Fouillat <m.fouillat@hotmail.fr>
2026-03-04 13:03:55 +0100

    added method handling

--- CSharp2TS.CLI/Generators/TSInterfaces/TSInterface.cs
+++ CSharp2TS.CLI/Generators/TSInterfaces/TSInterface.cs
@@ -5,8 +5,10 @@ namespace CSharp2TS.CLI.Generators.TSInterfaces {
         public string Name { get; private set; }
         public IList<TSImport> Imports { get; private set; } = [];
         public IList<TSInterfaceProperty> Properties { get; private set; } = [];
+        public IList<TSInterfaceMethod> Methods { get; private set; } = [];
         public IList<string> GenericParameters { get; set; } = [];
         public bool GenerateClass { get; set; }
+        public bool IncludeMethods { get; set; }
 
         public TSInterface(string name) {
             Name = name;
